bd4a6a0e2b05205ef8cdff2b0f9c8ad13132b027
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-07-21 16:13:03 -0500

    Create promote-adds.ps1
    Adding in actual script to repo so syntax can be examined without needing to download the zip file.

---
+++ DSC/promote-adds.ps1
@@ -0,0 +1,82 @@
+Configuration CreateADReplicaDC 
+{ 
+   param 
+    ( 
+        [Parameter(Mandatory)][String]$DomainName,
+        [Parameter(Mandatory)][System.Management.Automation.PSCredential]$safemodeAdminCreds,
+        [Parameter(Mandatory)][System.Management.Automation.PSCredential]$Admincreds,
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+    Import-DscResource -ModuleName xActiveDirectory, xPendingReboot, xStorage, PSDesiredStateConfiguration, xDSCDomainJoin
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$SafeCreds = New-Object System.Management.Automation.PSCredential ($safemodeAdminCreds.UserName, $safemodeAdminCreds.Password)
+
+    Node localhost
+    {
+       LocalConfigurationManager            
+       {            
+          ActionAfterReboot = 'ContinueConfiguration'            
+          ConfigurationMode = 'ApplyOnly'            
+          RebootNodeIfNeeded = $true            
+       }
+
+       WindowsFeature RSAT 
+       {
+          Ensure = "Present"
+          Name = "RSAT"
+       }
+
+        xWaitforDisk Disk2
+        {
+            DiskId = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskId = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        xDSCDomainjoin JoinDomain
+        {
+            Domain = $DomainName
+            Credential = $DomainCreds
+            DependsOn = "[xDisk]ADDataDisk"
+		}
+       WindowsFeature ADDSInstall 
+       { 
+          Ensure = "Present" 
+          Name = "AD-Domain-Services"
+          DependsOn = "[xDSCDomainJoin]JoinDomain"
+       }
+
+       xWaitForADDomain DscForestWait 
+       { 
+          DomainName = $DomainName 
+          DomainUserCredential= $DomainCreds
+          RetryCount = $RetryCount
+          RetryIntervalSec = $RetryIntervalSec
+          DependsOn = "[WindowsFeature]ADDSInstall"
+      }
+
+      xADDomainController ReplicaDC 
+      { 
+         DomainName = $DomainName 
+         DomainAdministratorCredential = $DomainCreds
+         SafemodeAdministratorPassword = $SafeCreds
+         DatabasePath = "F:\NTDS\Database"
+         LogPath = "F:\NTDS\Logs"
+         SysvolPath = "F:\SYSVOL"
+         DependsOn = "[xWaitForADDomain]DscForestWait"
+      }
+
+      xPendingReboot Reboot1
+      { 
+         Name = "RebootServer"
+         DependsOn = "[xADDomainController]ReplicaDC"
+      }
+   }
+}
